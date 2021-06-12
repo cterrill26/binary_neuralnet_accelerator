b44a8347e09ce369838cbbb0dcea9a2c8b95c7e4
Fred Chu <freddychu222@gmail.com>
2021-06-12 03:53:03 -0700

    Update add4.sv

--- add4.sv
+++ add4.sv
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 06/02/2021 12:13:09 AM
+// Create Date: 06/12/2021 12:15:19 AM
 // Design Name: 
 // Module Name: add4
 // Project Name: 
@@ -24,16 +24,16 @@ module add4
 #(parameter WIDTH_IN = 8)
 (
     input clk,
-    input signed [WIDTH_IN-1:0] in0, in1, in2, in3,
-    output logic signed [WIDTH_IN+2-1:0] out
+    input signed [WIDTH_IN+10:0] in [3:0],
+    output logic signed [WIDTH_IN+10:0] out
 );
 
-    logic signed [WIDTH_IN:0] sum0, sum1;
+    logic signed [WIDTH_IN+10:0] sum0, sum1;
     
     always_comb 
     begin
-        sum0 = in0 + in1;
-        sum1 = in2 + in3;
+        sum0 = in[0] + in[1];
+        sum1 = in[2] + in[3];
     end
     
     always_ff @ (posedge clk) 
